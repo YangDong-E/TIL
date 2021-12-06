b659a14098648ea885d1aa6a628d764c1587b059
YangDong-E <ehddh125@naver.com>
2021-12-06 16:54:50 +0900

    commit

--- 2021.12.03/src/컴포넌트선언문.js
+++ 2021.12.03/src/컴포넌트선언문.js
@@ -1,25 +1,25 @@
-// // 컴포넌트 선언 방법 1
+// // 컴포넌트 선언 방법 1 - 함수형
 
-// import React from "react";
-// import "./App.css";
+import React from "react";
+import "./App.css";
 
-// function App() {
-//   const name = "리액트";
-//   return <div className="react">{name}</div>;
-// }
+function App() {
+  const name = "리액트";
+  return <div className="react">{name}</div>;
+}
 
-// export default App;
+export default App;
 
-// // 컴포넌트 선언 방법 2
+// // 컴포넌트 선언 방법 2 - 클래스형
 
-// import React, {Component} from "react";
-// import "./App.css";
+import React, {Component} from "react";
+import "./App.css";
 
-// class App extends Component {
-//   render(){
-//     const name = "react";
-//     return <div className="react">{name}</div>
-//   }
-// }
+class App extends Component {
+  render(){
+    const name = "react";
+    return <div className="react">{name}</div>
+  }
+}
 
-// export default App;
+export default App;
