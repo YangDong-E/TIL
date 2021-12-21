8cf6efd815697f2b5efb1194b47e11cc878e0c96
YangDong-E <ehddh125@naver.com>
2021-12-21 18:07:51 +0900

    변경

--- 2021.12.09/src/App.js
+++ 2021.12.09/src/App.js
@@ -9,8 +9,9 @@ class App extends Component {
                 <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
                 {/* ref를 사용하여 내부메서드 가져오기 */}
                 <button onClick={() => this.scrollBox.scrollToBottom()}>
-                    {/* 문법상으로는 onClick={this.scrollBox.scrollToBottom} 같은 형식으로 해도 되지만 컴포넌트가 처음 렌더링 될때는 this.scrollBox 값이 undefined 이므로 this.scrollBox.scrollToBottom 값을 받아올때 오류가 발생하기때문에
-                화살표 함수를 사용하여 새로운 함수를 만들고 내부에서 메서드를 실행하면 버튼을 누를때 값을 읽어와서 실행하므로 오류가 발생하지 않는다. */}
+                    {/* 문법상으로는 onClick={this.scrollBox.scrollToBottom} 같은 형식으로 해도 되지만 
+                    컴포넌트가 처음 렌더링 될때는 this.scrollBox 값이 undefined 이므로 this.scrollBox.scrollToBottom 값을 받아올때 오류가 발생하기때문에
+                    화살표 함수를 사용하여 새로운 함수를 만들고 내부에서 메서드를 실행하면 버튼을 누를때 값을 읽어와서 실행하므로 오류가 발생하지 않는다. */}
                     맨밑으로
                 </button>
             </div>
