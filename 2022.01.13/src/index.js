000b00c2e913c315236f5e477bf08527c9a500af
YangDong-E <ehddh125@naver.com>
2022-01-13 19:42:08 +0900

    서버사이드 렌더링 (1)

--- 2022.01.13/src/index.js
+++ 2022.01.13/src/index.js
@@ -4,11 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import rootReducer from './modules';
+
+const store = createStore(
+    rootReducer,
+    window.__PRELOADED_STATE__,
+    applyMiddleware(thunk),
+);
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root'),
 );
 
