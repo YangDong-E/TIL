380017244d707450848d40d2071bbd5c152407c6
YangDong-E <ehddh125@naver.com>
2022-01-14 16:01:37 +0900

    서버 사이드 렌더링 (2)

--- 2022.01.14/src/index.js
+++ 2022.01.14/src/index.js
@@ -7,23 +7,43 @@ import { BrowserRouter } from 'react-router-dom';
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import rootReducer from './modules';
+import rootReducer, { rootSaga } from './modules';
+import createSagaMiddleware from 'redux-saga';
+import { loadableReady } from '@loadable/component';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
     rootReducer,
     window.__PRELOADED_STATE__,
-    applyMiddleware(thunk),
+    applyMiddleware(thunk, sagaMiddleware),
 );
 
-ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('root'),
-);
+sagaMiddleware.run(rootSaga);
 
+// 같은 내용을 쉽게 재사용할 수 있도록 렌더링할 내용을 하나의 컴포넌트로 묶음
+const Root = () => {
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    );
+};
+
+const root = document.getElementById('root');
+
+// 프로덕션 환경에서는 loadabelReady와 hydrate를 사용하고
+// 개발 환경에서는 기존 방식으로 처리
+
+if (process.env.NODE_ENV === 'production') {
+    loadableReady(() => {
+        ReactDOM.hydrate(<Root />, root);
+    });
+} else {
+    ReactDOM.render(<Root />, root);
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
