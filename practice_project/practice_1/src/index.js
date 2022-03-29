6219f77f0d1ab9ce892c34c4eba8c345cb894412
YangDong-E <ehddh125@naver.com>
2022-03-29 20:48:17 +0900

    2022.03.29 - 빙고판 숫자 채워넣기 실패

--- practice_project/practice_1/src/index.js
+++ practice_project/practice_1/src/index.js
@@ -1,17 +1,109 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import './index.css';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root'),
-);
+// let usedNums = new Array(25);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// function getNewNum(props) {
+//     // 25까지 랜덤수 뽑기
+//     let num = Math.floor(Math.random() * 25);
+//     // ID square + 숫자
+//     let currSquare = 'square' + props;
+//     // index 수
+//     let newNum;
+//     // 빈배열 25 만들기
+//     let Arr = new Array(25);
+
+//     for (let props = 0; props < Arr.length; props++) {
+//         newNum = Arr[props] * 15 + num + 1;
+//         return newNum;
+//     }
+
+//     document.getElementById('square0').innerHTML = num;
+//     document.getElementById('square1').innerHTML = num;
+//     document.getElementById('square2').innerHTML = num;
+//     document.getElementById('square3').innerHTML = num;
+//     document.getElementById('square4').innerHTML = num;
+//     document.getElementById('square5').innerHTML = num;
+//     document.getElementById('square6').innerHTML = num;
+//     document.getElementById('square7').innerHTML = num;
+//     document.getElementById('square8').innerHTML = num;
+//     document.getElementById('square9').innerHTML = num;
+//     document.getElementById('square10').innerHTML = num;
+//     document.getElementById('square11').innerHTML = num;
+//     document.getElementById('square12').innerHTML = num;
+//     document.getElementById('square13').innerHTML = num;
+//     document.getElementById('square14').innerHTML = num;
+//     document.getElementById('square15').innerHTML = num;
+//     document.getElementById('square16').innerHTML = num;
+//     document.getElementById('square17').innerHTML = num;
+//     document.getElementById('square18').innerHTML = num;
+//     document.getElementById('square19').innerHTML = num;
+//     document.getElementById('square20').innerHTML = num;
+//     document.getElementById('square21').innerHTML = num;
+//     document.getElementById('square22').innerHTML = num;
+//     document.getElementById('square23').innerHTML = num;
+//     document.getElementById('square24').innerHTML = num;
+// }
+
+// const btn = document.getElementById('btn');
+// btn.addEventListener('click', getNewNum);
+
+var usedNums = new Array(26);
+
+function newCard() {
+    //Starting loop through each square card
+    for (var i = 0; i < 24; i++) {
+        //<--always this code for loops. change in red
+        setSquare(i);
+    }
+}
+
+function setSquare(thisSquare) {
+    var currSquare = 'square' + thisSquare;
+    var newNum;
+
+    var colPlace = new Array(
+        0,
+        1,
+        2,
+        3,
+        4,
+        0,
+        1,
+        2,
+        3,
+        4,
+        0,
+        1,
+        3,
+        4,
+        0,
+        1,
+        2,
+        3,
+        4,
+        0,
+        1,
+        2,
+        3,
+        4,
+    );
+
+    do {
+        newNum = colPlace[thisSquare] * 15 + getNewNum() + 1;
+    } while (usedNums[newNum]);
+
+    usedNums[newNum] = true;
+    document.getElementById(currSquare).innerHTML = newNum;
+}
+
+function getNewNum() {
+    return Math.floor(Math.random() * 25);
+}
+
+function anotherCard() {
+    for (var i = 1; i < usedNums.length; i++) {
+        usedNums[i] = false;
+    }
+
+    newCard();
+}
