846815cf6c1329f891b7d652624b5b685c77170f
YangDong-E <ehddh125@naver.com>
2022-04-24 19:10:35 +0900

    2022.04.24 - 컴포넌트 성능 최적화 완료

--- 2022.04.21/src/App.js
+++ 2022.04.21/src/App.js
@@ -1,67 +1,94 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useReducer, useRef } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
+function createBulkTodos() {
+    const array = [];
+    for (let i = 1; i <= 2500; i++) {
+        array.push({
+            id: i,
+            text: `할 일 ${i}`,
+            checked: false,
+        });
+    }
+    return array;
+}
+
+function todoReducer(todos, action) {
+    switch (action.type) {
+        case 'INSERT': // 새로 추가
+            // {type: 'INSERT', todo: {id: 1, text: 'todo', checked: false}}
+            return todos.concat(action.todo);
+        case 'REMOVE': // 제거
+            // {type: 'REMOVE, id: 1}
+            return todos.filter((todo) => todo.id !== action.id);
+        case 'TOGGLE': // 토글(수정)
+            // {type: 'TOGGLE', id:1}
+            return todos.map((todo) =>
+                todo.id === action.id
+                    ? { ...todo, checked: !todo.checked }
+                    : todo
+            );
+        default:
+            return todos;
+    }
+}
+
 const App = () => {
     // useState를 통한 상태관리
-    const [todos, setTodos] = useState([
-        {
-            id: 1,
-            text: '리액트의 기초 알아보기',
-            checked: true,
-        },
-        {
-            id: 2,
-            text: '컴포넌트 스타일링해 보기',
-            checked: true,
-        },
-        {
-            id: 3,
-            text: '일정 관리 앱 만들어 보기',
-            checked: false,
-        },
-    ]);
+    // const [todos, setTodos] = useState(createBulkTodos);
+
+    // useReducer를 통한 성능최적화 상태관리
+    const [todos, dispatch] = useReducer(
+        todoReducer,
+        undefined,
+        createBulkTodos
+    );
 
     // 고윳값으로 사용될 id
     // ref를 사용하여 변수 담기
     // useRef(4)에서 4는 위 코드 todos에 있는 요소의 id가 3까지 생성되어있기때문에 4를 주었다.
-    const nextId = useRef(4);
+    const nextId = useRef(2501);
 
     // useCallback을 사용하여 함수를 재사용한다.
-    const onInsert = useCallback(
-        (text) => {
-            const todo = {
-                id: nextId.current,
-                text,
-                checked: false,
-            };
-            // concat을 사용하여 추가
-            setTodos(todos.concat(todo));
-            nextId.current += 1; // nextId 1씩 더하기
-        },
-        [todos]
-    );
+    const onInsert = useCallback((text) => {
+        const todo = {
+            id: nextId.current,
+            text,
+            checked: false,
+        };
+        // concat을 사용하여 추가
+        // useState의 함수형 업데이트를 사용한 컴포넌트 성능 최적화
+        // setTodos((todos) => todos.concat(todo));
 
-    const onRemove = useCallback(
-        (id) => {
-            // filter를 사용하여 id를 기준으로 삭제
-            setTodos(todos.filter((todo) => todo.id !== id));
-        },
-        [todos]
-    );
+        // useReducer를 사용한 컴포넌트 성능 최적화 (dispatch)
+        dispatch({ type: 'INSERT', todo });
+        nextId.current += 1; // nextId 1씩 더하기
+    }, []);
 
-    const onToggle = useCallback(
-        (id) => {
-            setTodos(
-                // map을 사용하여 id와 checked로 요소의 체크요소를 확인한다.
-                todos.map((todo) =>
-                    todo.id === id ? { ...todo, checked: !todo.checked } : todo
-                )
-            );
-        },
-        [todos]
-    );
+    const onRemove = useCallback((id) => {
+        // filter를 사용하여 id를 기준으로 삭제
+        // useState의 함수형 업데이트를 사용한 컴포넌트 성능 최적화
+        // setTodos((todos) => todos.filter((todo) => todo.id !== id));
+
+        // useReducer를 사용한 컴포넌트 성능 최적화 (dispatch)
+        dispatch({ type: 'REMOVE', id });
+    }, []);
+
+    const onToggle = useCallback((id) => {
+        // useState의 함수형 업데이트를 사용한 컴포넌트 성능 최적화
+        // setTodos(
+        //     // map을 사용하여 id와 checked로 요소의 체크요소를 확인한다.
+        //     (todos) =>
+        //         todos.map((todo) =>
+        //             todo.id === id ? { ...todo, checked: !todo.checked } : todo
+        //         )
+        // );
+
+        // useReducer를 사용한 컴포넌트 성능 최적화 (dispatch)
+        dispatch({ type: 'TOGGLE', id });
+    }, []);
     return (
         <TodoTemplate>
             <TodoInsert onInsert={onInsert} />
--- 2022.04.21/src/components/TodoListItem.js
+++ 2022.04.21/src/components/TodoListItem.js
@@ -8,29 +8,32 @@ import cn from 'classnames';
 import './TodoListItem.scss';
 
 // todo, onRemove, onToggle을 props로 받는다.
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
     // todo안에 값 중 id, text, checked 값을 사용한다.
     const { id, text, checked } = todo;
 
     return (
-        <div className="TodoListItem">
-            {/* 조건부 스타일링을 위해 classnames(cs)를 사용한다. */}
-            <div
-                className={cn('checkbox', { checked })}
-                // 클릭을 했을 시 onToggle이 id값에 따라 실행한다.
-                onClick={() => onToggle(id)}
-            >
-                {/* 삼항 연사자를 사용하여 checked의 true false 값에 따라 아이콘에 변화를 준다. */}
-                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-                {/* todo 데이터에서 text값을 출력한다. */}
-                <div className="text">{text}</div>
-            </div>
-            {/* 클릭을 했을 시 onRemove가 id값에 따라 실행한다. */}
-            <div className="remove" onClick={() => onRemove(id)}>
-                <MdRemoveCircleOutline />
+        <div className="TodoListItem-virtualized" style={style}>
+            <div className="TodoListItem">
+                {/* 조건부 스타일링을 위해 classnames(cs)를 사용한다. */}
+                <div
+                    className={cn('checkbox', { checked })}
+                    // 클릭을 했을 시 onToggle이 id값에 따라 실행한다.
+                    onClick={() => onToggle(id)}
+                >
+                    {/* 삼항 연사자를 사용하여 checked의 true false 값에 따라 아이콘에 변화를 준다. */}
+                    {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+                    {/* todo 데이터에서 text값을 출력한다. */}
+                    <div className="text">{text}</div>
+                </div>
+                {/* 클릭을 했을 시 onRemove가 id값에 따라 실행한다. */}
+                <div className="remove" onClick={() => onRemove(id)}>
+                    <MdRemoveCircleOutline />
+                </div>
             </div>
         </div>
     );
 };
 
-export default TodoListItem;
+// React.memo를 사용하여 컴포넌트 성능 최적화
+export default React.memo(TodoListItem);
