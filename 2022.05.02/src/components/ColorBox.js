9b62be21b38f47e6024aa1b964a25a54872e187e
YangDong-E <ehddh125@naver.com>
2022-05-02 17:03:38 +0900

    2022.05.02 - Context AP(Hook 사용전)

---
+++ 2022.05.02/src/components/ColorBox.js
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react';
+import ColorContext, { ColorConsumer } from '../contexts/color';
+
+const ColorBox = () => {
+    const { state } = useContext(ColorContext);
+    return (
+        <ColorConsumer>
+            {({ state }) => (
+                <>
+                    <div
+                        style={{
+                            width: '64px',
+                            height: '64px',
+                            background: state.color,
+                        }}
+                    />
+                    <div
+                        style={{
+                            width: '32px',
+                            height: '32px',
+                            background: state.subcolor,
+                        }}
+                    />
+                </>
+            )}
+        </ColorConsumer>
+    );
+};
+
+export default ColorBox;
