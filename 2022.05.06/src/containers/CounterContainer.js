18e3819e4a10840594aac5919168fa843f4d177e
YangDong-E <ehddh125@naver.com>
2022-05-06 18:15:05 +0900

    리덕스 사용하여 리액트 애플리케이션 상태관리(Counter)

---
+++ 2022.05.06/src/containers/CounterContainer.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import Counter from '../components/Counter';
+import { decrease, increase } from '../modules/counter';
+
+const CounterContainer = ({ number, increase, decrease }) => {
+    return (
+        <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    );
+};
+
+// 임시 함수를 선언하면서 디스패치 하는 방법
+
+// 1. connect 함수를 사용하며 외부에 미리 선언
+
+// const mapStateToProps = (state) => ({
+//     number: state.counter.number,
+// });
+// const mapDispatchToProps = (dispatch) => ({
+//     // 임시 함수
+//     increase: () => {
+//         dispatch(increase());
+//     },
+//     decrease: () => {
+//         dispatch(decrease());
+//     },
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+
+// 2. connect 함수 내부에 익명 함수 형태로 선언
+
+// export default connect(
+//     (state) => ({
+//         number: state.counter.number,
+//     }),
+//     (dispatch) => ({
+//         increase: () => dispatch(increase()),
+//         decrease: () => dispatch(decrease()),
+//     })
+// )(CounterContainer);
+
+// 3. bindActionCreators 사용
+// export default connect(
+//     (state) => ({
+//         number: state.counter.number,
+//     }),
+//     (dispatch) =>
+//         bindActionCreators(
+//             {
+//                 increase,
+//                 decrease,
+//             },
+//             dispatch
+//         )
+// )(CounterContainer);
+
+// 4. 액션 생성 함수로 이루어진 객체 형태로 넣어 주는 방식
+export default connect(
+    (state) => ({
+        number: state.counter.number,
+    }),
+    {
+        increase,
+        decrease,
+    }
+)(CounterContainer);
---
+++ 2022.05.06/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { legacy_createStore as createStore } from 'redux';
+import './index.css';
+import App from './App';
+import rootReducer from './modules';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
