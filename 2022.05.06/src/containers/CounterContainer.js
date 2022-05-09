ccf0061620f67fd308789c6f3c1e9f4e8e555db8
YangDong-E <ehddh125@naver.com>
2022-05-09 21:53:17 +0900

    2022.05.09 - 리덕스를 사용하여 리액트 애플리케이션 상태관리

--- 2022.05.06/src/containers/CounterContainer.js
+++ 2022.05.06/src/containers/CounterContainer.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Counter from '../components/Counter';
 import { decrease, increase } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+    const number = useSelector((state) => state.counter.number);
+    const dispatch = useDispatch();
+
+    // useCallback 사용으로 최적화
+    const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+    const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
     return (
-        <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+        <Counter
+            number={number}
+            onIncrease={onIncrease}
+            onDecrease={onDecrease}
+        />
     );
 };
 
@@ -56,12 +66,4 @@ const CounterContainer = ({ number, increase, decrease }) => {
 // )(CounterContainer);
 
 // 4. 액션 생성 함수로 이루어진 객체 형태로 넣어 주는 방식
-export default connect(
-    (state) => ({
-        number: state.counter.number,
-    }),
-    {
-        increase,
-        decrease,
-    }
-)(CounterContainer);
+export default CounterContainer;
