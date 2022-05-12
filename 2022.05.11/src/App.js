b9df392a5097ec82af75158bd09d6306d75a373f
YangDong-E <ehddh125@naver.com>
2022-05-12 19:02:13 +0900

    2022.05.12 - 리팩토링 전

--- 2022.05.11/src/App.js
+++ 2022.05.11/src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
-import CounterContainer from "./containers/CounterContainer";
+import React from 'react';
+import CounterContainer from './containers/CounterContainer';
+import SampleContainer from './containers/SampleContainer';
 
 const App = () => {
-  return (
-    <div>
-      <CounterContainer />
-    </div>
-  );
+    return (
+        <div>
+            {/* <CounterContainer /> */}
+            <SampleContainer />
+        </div>
+    );
 };
 
 export default App;
