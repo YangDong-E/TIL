3ddb09757e2ca25fa51514432bf398bf926279ba
YangDong-E <ehddh125@naver.com>
2022-05-17 18:09:15 +0900

    2022.05.17 - Saga 사용전

--- 2022.05.16/src/index.js
+++ 2022.05.16/src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import rootReducer from './modules';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+    rootReducer,
+    window.__PRELOADED_STATE__,
+    applyMiddleware(thunk)
+);
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 );
