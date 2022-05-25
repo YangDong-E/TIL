c29bb0502402a8cdc5290a1ebb5581a77dc4587a
YangDong-E <ehddh125@naver.com>
2022-05-25 19:37:49 +0900

    2022.05.25 - MongoDB(스키마 전)

--- 2022.05.19/blog-backend/src/api/posts/index.js
+++ 2022.05.19/blog-backend/src/api/posts/index.js
@@ -1,15 +1,14 @@
-const Router = require('koa-router');
-
-// const 모듈 이름 = require('파일이름');
-// 모듈이름.이름(); 형식으로 불러올 수 있다.
-const postsCtrl = require('./posts.ctrl');
+import Router from 'koa-router';
+import * as postsCtrl from './posts.ctrl';
 
 const posts = new Router();
 
+// 모듈이름.이름(); 형식으로 불러올 수 있다.
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
 posts.get('/:id', postsCtrl.read);
 posts.delete('/:id', postsCtrl.remove);
 posts.put('/:id', postsCtrl.replace);
 posts.patch('/:id', postsCtrl.update);
-module.exports = posts;
+
+export default posts;
--- 2022.05.19/blog-backend/src/api/posts/posts.ctrl.js
+++ 2022.05.19/blog-backend/src/api/posts/posts.ctrl.js
@@ -13,8 +13,8 @@ const posts = [
 Post /api/posts
 { title, body }
 */
-// exports.이름 = 형식으로 함수를 내보낸다.
-exports.write = (ctx) => {
+// export const 이름 = 형식으로 함수를 내보낸다.
+export const write = (ctx) => {
     // REST API의 Request Body는 ctx.request.body에서 조회할 수 있습니다.
     const { title, body } = ctx.request.body;
     postId += 1; // 기존 postId 값에 1을 더합니다.
@@ -27,7 +27,7 @@ exports.write = (ctx) => {
 GET /api/posts
 */
 
-exports.list = (ctx) => {
+export const list = (ctx) => {
     ctx.body = posts;
 };
 
@@ -35,7 +35,7 @@ exports.list = (ctx) => {
 GET /api/posts/:id
 */
 
-exports.read = (ctx) => {
+export const read = (ctx) => {
     const { id } = ctx.params;
     // 주어진 id 값으로 포스트를 찾습니다.
     // 파라미터로 받아 온 값은 문자열 형식이므로 파라미터를 숫자로 변환하거나
@@ -56,7 +56,7 @@ exports.read = (ctx) => {
 DELETE /api/posts/:id
 */
 
-exports.remove = (ctx) => {
+export const remove = (ctx) => {
     const { id } = ctx.params;
     // 해당 id를 가진 post가 몇 번째인지 확인합니다.
     const index = posts.findIndex((p) => p.id.toString() === id);
@@ -78,7 +78,7 @@ PUT /api/posts/:id
 { title, body }
 */
 
-exports.replace = (ctx) => {
+export const replace = (ctx) => {
     // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용합니다.
     const { id } = ctx.params;
     // 해당 id를 가진 post가 몇 번째인지 확인합니다.
@@ -105,7 +105,7 @@ PATCH /api/posts/:id
 { title, body }
 */
 
-exports.update = (ctx) => {
+export const update = (ctx) => {
     // PATCH 메서드는 주어진 필드만 교체합니다.
     const { id } = ctx.params;
     // 해당 id를 가진 post가 몇 번째인지 확인합니다.
