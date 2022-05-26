ba91948ab9277adda0879ae291d55fae5f045af2
YangDong-E <ehddh125@naver.com>
2022-05-26 18:32:49 +0900

    2022.05.26 - MongoDB 연동

--- 2022.05.19/blog-backend/src/api/posts/index.js
+++ 2022.05.19/blog-backend/src/api/posts/index.js
@@ -6,9 +6,19 @@ const posts = new Router();
 // 모듈이름.이름(); 형식으로 불러올 수 있다.
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.put('/:id', postsCtrl.replace);
-posts.patch('/:id', postsCtrl.update);
+
+const post = new Router(); // /api/posts/:id
+// 코드 리팩토링 후
+
+post.get('/', postsCtrl.read);
+post.delete('/', postsCtrl.remove);
+post.patch('/', postsCtrl.update);
+
+posts.use('/:id', postsCtrl.checkObjectId, post.routes());
+
+// 코드 리팩토링 전
+// posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
+// posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
+// posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
 
 export default posts;
