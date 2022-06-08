96c524dd60715258a5aaa2d5688a1d6e6e5c8a0e
YangDong-E <ehddh125@naver.com>
2022-06-08 18:42:34 +0900

    2022.06.08 - 로그인 폼 구현중 (에러발생)

--- 2022.05.19/blog-frontend/src/index.js
+++ 2022.05.19/blog-frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'redux';
+import { Provider } from 'react-redux';
 import { legacy_createStore as createStore } from 'redux';
 import rootReducer from './modules';
 import { composeWithDevTools } from 'redux-devtools-extension';
