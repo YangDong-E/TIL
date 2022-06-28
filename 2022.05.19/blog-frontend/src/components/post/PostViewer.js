3435ee76973d9442529d5df0ceb7900d03c1ddbc
YangDong-E <ehddh125@naver.com>
2022-06-28 17:50:03 +0900

    2022.06.28 - 프론트엔드 구현중 포스트조회부분 에러발생(추후에 경력을 키운뒤 완성할 예정)

--- 2022.05.19/blog-frontend/src/components/post/PostViewer.js
+++ 2022.05.19/blog-frontend/src/components/post/PostViewer.js
@@ -2,11 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 import Responsive from '../common/Responsive';
+import SubInfo from '../common/SubInfo';
+import Tags from '../common/Tags';
 
 const PostViewerBlock = styled(Responsive)`
     margin-top: 4rem;
 `;
-
 const PostHead = styled.div`
     border-bottom: 1px solid ${palette.gray[2]};
     padding-bottom: 3rem;
@@ -15,32 +16,6 @@ const PostHead = styled.div`
         font-size: 3rem;
         line-height: 1.5;
         margin: 0;
-    }
-`;
-
-const SubInfo = styled.div`
-    margin-top: 1rem;
-    color: ${palette.gray[6]};
-
-    /* span 사이에 가운뎃점 문자 보여 주기 */
-    span + span:before {
-        color: ${palette.gray[5]};
-        padding-left: 0.25rem;
-        padding-right: 0.25rem;
-        content: '\\B7'; /* 가운뎃점 문자 */
-    }
-`;
-
-const Tags = styled.div`
-    margin-top: 0.5rem;
-    .tag {
-        display: inline-block;
-        color: ${palette.cyan[7]};
-        text-decoration: none;
-        margin-right: 0.5rem;
-        &:hover {
-            color: ${palette.cyan[6]};
-        }
     }
 `;
 
@@ -60,32 +35,24 @@ const PostViewer = ({ post, error, loading }) => {
         return <PostViewerBlock>오류 발생!</PostViewerBlock>;
     }
 
-    // 로딩 중이거나 아직 포스트 데이터가 없을 때
+    // 로딩중이거나, 아직 포스트 데이터가 없을 시
     if (loading || !post) {
         return null;
     }
 
-    const { title, body, user, publishedDate, tags } = post;
+    const { title, body, username, publishedDate, tags } = post;
     return (
         <PostViewerBlock>
             <PostHead>
                 <h1>{title}</h1>
-                <SubInfo>
-                    <span>
-                        <b>{user.username}</b>
-                    </span>
-                    <span>{new Date(publishedDate).toLocaleDateString()}</span>
-                </SubInfo>
-                <Tags>
-                    {tags &&
-                        tags.map((tag) => <div className="tag">#{tag}</div>)}
-                </Tags>
+                <SubInfo
+                    username={username}
+                    publishedDate={publishedDate}
+                    hasMarginTop
+                />
+                <Tags tags={tags} />
             </PostHead>
-            <PostContent
-                dangerouslySetInnerHTML={{
-                    __html: body,
-                }}
-            />
+            <PostContent dangerouslySetInnerHTML={{ __html: body }} />
         </PostViewerBlock>
     );
 };
