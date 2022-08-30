06e9aacf3dcab64594876d382a2af2d4a4e16fba
YangDong-E <ehddh125@naver.com>
2022-08-30 19:16:17 +0900

    2022.08.30 - 배열 다루기

---
+++ TypeScript/09.람다라이브러리/09-9/src/model/person/makeRandomIPerson.ts
@@ -0,0 +1,8 @@
+import { makeIPerson, IPerson } from './makeIPerson';
+import { makeRandomILocation } from '../location';
+import Chance from 'chance';
+
+const c = new Chance();
+
+export const makeRandomIPerson = (): IPerson =>
+    makeIPerson(c.name(), c.age(), c.profession(), makeRandomILocation());
